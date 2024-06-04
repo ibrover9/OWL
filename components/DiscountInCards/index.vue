b3995af161ee110ec9e60ce9ea914dbfdb8fadcd
Artur Novikov <artur.20177102@mail.ru>
2024-06-04 21:24:47 +0300

    final-final

---
+++ components/DiscountInCards/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="discount">
+    <span class="gray">акция</span>
+    <div class="red">
+      <div class="percent">%</div>
+      акции
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+@import "../../assets/DiscountInCards.less";
+</style>
